e1bd5ff8f2d2e57e817c059664daf093363a6ad3
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-05-16 20:43:38 +0200

    backup - load crontab from app config

---
+++ Warehouse.Server.Data/MongoContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using MongoDB.Driver;
+using Warehouse.Server.Models;
+
+namespace Warehouse.Server.Data
+{
+    public class MongoContext : IMongoContext
+    {
+        private readonly MongoDatabase database;
+
+        public MongoContext()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            database = server.GetDatabase("skill");
+        }
+
+        public MongoDatabase Database { get { return database; } }
+
+        public MongoCollection<Product> Products
+        {
+            get { return database.GetCollection<Product>("products"); }
+        }
+
+        public MongoCollection<User> Users
+        {
+            get { return database.GetCollection<User>("users"); }
+        }
+    }
+}
---
+++ Warehouse.Server.Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Warehouse.Server.Models
+{
+    [BsonIgnoreExtraElements]
+    public class User
+    {
+        [BsonElement("UserName")]
+        public string UserName { get; set; }
+
+        [BsonElement("Roles")]
+        public string[] Roles { get; set; }
+
+        public string Password { get; set; }
+    }
+}
