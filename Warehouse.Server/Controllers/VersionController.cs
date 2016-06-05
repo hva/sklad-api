e1445273b17560593a0dc54e7318b1fc0c4987d5
Yauheni Khvaliuk <hvaluk@gmail.com>
2016-06-05 14:31:14 +0200

    version controller

---
+++ Warehouse.Server/Controllers/VersionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace Warehouse.Server.Controllers
+{
+    public class VersionController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var path = HostingEnvironment.MapPath("~/COMMIT");
+            if (string.IsNullOrEmpty(path))
+            {
+                return InternalServerError();
+            }
+
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var version = sr.ReadLine();
+                    var commit = sr.ReadLine();
+                    var data = new { version, commit };
+                    return Ok(data);
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
