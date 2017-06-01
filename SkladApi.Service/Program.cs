12289d101d586f80c88c825eb6abf6bf9802d378
Yauheni Khvaliuk <hvaluk@gmail.com>
2017-06-01 23:28:27 +0200

    start self hosted api

---
+++ SkladApi.Service/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace SkladApi.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:8000")))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:8000");
+                Console.ReadLine();
+            }
+        }
+    }
+}
